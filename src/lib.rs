b316c8502f655ca3fe4db84fb5ef4dbe8815183c
ppoage <poagepatrick@gmail.com>
2024-09-19 22:12:18 -0700

    Features?

--- src/lib.rs
+++ src/lib.rs
@@ -1,114 +1,203 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyDict};
-use std::sync::{Arc, Mutex};
-use std::thread;
+use pyo3::types::{PyBytes, PyDict, PyList};
 use pnet::datalink::{self, Channel::Ethernet};
-use pnet::packet::ethernet::EthernetPacket;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
 use pnet::packet::Packet;
+use pnet::packet::ip::IpNextHeaderProtocols;
+
 
 #[pyclass]
 struct PacketSniffer {
     interface_name: String,
-    stop_flag: Arc<Mutex<bool>>,
 }
 
 #[pymethods]
 impl PacketSniffer {
     #[new]
     fn new(interface_name: String) -> Self {
-        PacketSniffer {
-            interface_name,
-            stop_flag: Arc::new(Mutex::new(false)),
-        }
+        PacketSniffer { interface_name }
     }
-    
-    fn sniff(&self, py: Python, callback: PyObject) -> PyResult<()> {
-        let stop_flag = self.stop_flag.clone();
-        let interface_name = self.interface_name.clone();
-        let callback = callback.clone_ref(py);
 
-        // Start sniffing in a new thread
-        thread::spawn(move || {
-            if let Err(e) = capture_packets(&interface_name, stop_flag, callback) {
-                eprintln!("Error capturing packets: {}", e);
+    #[pyo3(signature = (
+        num_packets,
+        *,
+        protocol = None,
+        src_mac = None,
+        dst_mac = None,
+        src_ip = None,
+        dst_ip = None
+    ))]
+    fn capture_packets(
+        &self,
+        py: Python,
+        num_packets: usize,
+        protocol: Option<&str>,
+        src_mac: Option<&str>,
+        dst_mac: Option<&str>,
+        src_ip: Option<&str>,
+        dst_ip: Option<&str>,
+    ) -> PyResult<PyObject> {
+        // Find the network interface
+        let interface = datalink::interfaces()
+            .into_iter()
+            .find(|iface| iface.name == self.interface_name)
+            .ok_or_else(|| {
+                PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
+                    "No such network interface: {}",
+                    self.interface_name
+                ))
+            })?;
+
+        // Create a channel to receive on
+        let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+            Ok(Ethernet(_tx, rx)) => (_tx, rx),
+            Ok(_) => {
+                return Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "Unhandled channel type",
+                ))
             }
-        });
+            Err(e) => {
+                return Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
+                    "Unable to create channel: {}",
+                    e
+                )))
+            }
+        };
 
-        Ok(())
-    }
+        let mut packets = Vec::new();
 
-    fn stop(&self) {
-        let mut stop = self.stop_flag.lock().unwrap();
-        *stop = true;
+        while packets.len() < num_packets {
+            match rx.next() {
+                Ok(packet) => {
+                    // Parse the Ethernet packet
+                    if let Some(ethernet) = EthernetPacket::new(packet) {
+                        // Apply MAC address filters
+                        if let Some(src_mac_filter) = src_mac {
+                            if ethernet.get_source().to_string() != src_mac_filter {
+                                continue;
+                            }
+                        }
+                        if let Some(dst_mac_filter) = dst_mac {
+                            if ethernet.get_destination().to_string() != dst_mac_filter {
+                                continue;
+                            }
+                        }
+
+                        match ethernet.get_ethertype() {
+                            EtherTypes::Ipv4 => {
+                                // Parse IPv4 packet
+                                if let Some(ipv4_packet) = Ipv4Packet::new(ethernet.payload()) {
+                                    // Apply IP address filters
+                                    if let Some(src_ip_filter) = src_ip {
+                                        if ipv4_packet.get_source().to_string() != src_ip_filter {
+                                            continue;
+                                        }
+                                    }
+                                    if let Some(dst_ip_filter) = dst_ip {
+                                        if ipv4_packet.get_destination().to_string()
+                                            != dst_ip_filter
+                                        {
+                                            continue;
+                                        }
+                                    }
+
+                                    let next_proto = ipv4_packet.get_next_level_protocol();
+
+                                    // Apply protocol filter
+                                    if let Some(proto_filter) = protocol {
+                                        if proto_filter.eq_ignore_ascii_case("TCP")
+                                            && next_proto != IpNextHeaderProtocols::Tcp
+                                        {
+                                            continue;
+                                        } else if proto_filter.eq_ignore_ascii_case("UDP")
+                                            && next_proto != IpNextHeaderProtocols::Udp
+                                        {
+                                            continue;
+                                        }
+                                    }
+
+                                    // Parse transport layer if needed
+                                    let transport_payload = if next_proto == IpNextHeaderProtocols::Tcp {
+                                        if let Some(tcp_packet) = TcpPacket::new(ipv4_packet.payload()) {
+                                            tcp_packet.payload().to_vec()
+                                        } else {
+                                            continue;
+                                        }
+                                    } else if next_proto == IpNextHeaderProtocols::Udp {
+                                        if let Some(udp_packet) = UdpPacket::new(ipv4_packet.payload()) {
+                                            udp_packet.payload().to_vec()
+                                        } else {
+                                            continue;
+                                        }
+                                    } else {
+                                        ipv4_packet.payload().to_vec()
+                                    };
+
+                                    // Prepare packet data
+                                    let packet_info = create_packet_info(
+                                        py,
+                                        &ethernet,
+                                        Some(&ipv4_packet),
+                                        &transport_payload,
+                                    )?;
+                                    packets.push(packet_info);
+                                }
+                            }
+                            EtherTypes::Ipv6 => {
+                                // Handle IPv6 if needed
+                                continue;
+                            }
+                            _ => {
+                                // Other EtherTypes
+                                continue;
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    return Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
+                        "An error occurred while reading: {}",
+                        e
+                    )));
+                }
+            }
+        }
+
+        let py_packets = PyList::new_bound(py, packets);
+        Ok(py_packets.into())
     }
 }
 
-fn capture_packets(
-    interface_name: &str,
-    stop_flag: Arc<Mutex<bool>>,
-    callback: PyObject,
-) -> Result<(), String> {
-    // Find the network interface
-    let interface = datalink::interfaces()
-        .into_iter()
-        .find(|iface| iface.name == interface_name)
-        .ok_or_else(|| format!("No such network interface: {}", interface_name))?;
+fn create_packet_info(
+    py: Python,
+    ethernet: &EthernetPacket,
+    ipv4: Option<&Ipv4Packet>,
+    payload: &[u8],
+) -> PyResult<PyObject> {
+    let packet_info = PyDict::new_bound(py);
+    packet_info.set_item("src_mac", ethernet.get_source().to_string())?;
+    packet_info.set_item("dst_mac", ethernet.get_destination().to_string())?;
+    packet_info.set_item("ethertype", format!("{:?}", ethernet.get_ethertype()))?;
 
-    // Create a channel to receive on
-    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(_tx, rx)) => (_tx, rx),
-        Ok(_) => return Err("Unhandled channel type".to_string()),
-        Err(e) => return Err(format!("Unable to create channel: {}", e)),
-    };
-
-    // Packet capturing loop
-    loop {
-        {
-            let stop = stop_flag.lock().unwrap();
-            if *stop {
-                break;
-            }
-        }
-
-        match rx.next() {
-            Ok(packet) => {
-                if let Some(ethernet) = EthernetPacket::new(packet) {
-                    // Prepare packet data
-                    let src_mac = ethernet.get_source().to_string();
-                    let dst_mac = ethernet.get_destination().to_string();
-                    let ethertype = format!("{:?}", ethernet.get_ethertype());
-                    let payload = ethernet.payload().to_vec();
-
-                    // Call the Python callback with packet info
-                    Python::with_gil(|py| {
-                        let packet_info = PyDict::new_bound(py);
-                        packet_info.set_item("src_mac", src_mac).unwrap();
-                        packet_info.set_item("dst_mac", dst_mac).unwrap();
-                        packet_info.set_item("ethertype", ethertype).unwrap();
-                        packet_info
-                            .set_item("payload", PyBytes::new_bound(py, &payload))
-                            .unwrap();
-
-                        if let Err(e) = callback.call1(py, (packet_info,)) {
-                            eprintln!("Callback error: {:?}", e);
-                        }
-                    });
-                } else {
-                    eprintln!("Invalid Ethernet packet");
-                }
-            }
-            Err(e) => {
-                eprintln!("An error occurred while reading: {}", e);
-            }
-        }
+    if let Some(ipv4_packet) = ipv4 {
+        packet_info.set_item("src_ip", ipv4_packet.get_source().to_string())?;
+        packet_info.set_item("dst_ip", ipv4_packet.get_destination().to_string())?;
+        packet_info.set_item(
+            "protocol",
+            format!("{:?}", ipv4_packet.get_next_level_protocol()),
+        )?;
     }
+    packet_info.set_item("payload", PyBytes::new_bound(py, payload))?;
 
-    Ok(())
+    Ok(packet_info.into())
 }
 
 #[pyfunction]
 fn list_interfaces() -> PyResult<Vec<String>> {
-    let interfaces = pnet::datalink::interfaces();
+    let interfaces = datalink::interfaces();
     let interface_names = interfaces
         .into_iter()
         .map(|iface| iface.name)
@@ -117,7 +206,7 @@ fn list_interfaces() -> PyResult<Vec<String>> {
 }
 
 #[pymodule]
-fn py_pnet( m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn pnet_sniffer(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PacketSniffer>()?;
     m.add_function(wrap_pyfunction!(list_interfaces, m)?)?;
     Ok(())
--- test.py
+++ test.py
@@ -1,47 +1,32 @@
-import ctypes
 import os
 import platform
-import sys
-import time
+import pnet_sniffer
 
+# Add System32 to the DLL search path on Windows
 if platform.system() == 'Windows':
     if platform.architecture()[0] == '64bit':
         system_dir = os.path.join(os.environ['SystemRoot'], 'System32')
     else:
         system_dir = os.path.join(os.environ['SystemRoot'], 'SysWOW64')
+    os.add_dll_directory(system_dir)
 
-    # Load the DLLs
-    ctypes.windll.LoadLibrary(os.path.join(system_dir, 'Packet.dll'))
-    ctypes.windll.LoadLibrary(os.path.join(system_dir, 'wpcap.dll'))
+def main():
+    # List available interfaces
+    interfaces = pnet_sniffer.list_interfaces()
+    print("Available interfaces:")
+    for iface in interfaces:
+        print(f"- {iface}")
 
+    # Replace with your interface name
+    interface_name = "Ethernet"  # Update this to match your interface
 
-print(sys.version)
-sys.path.insert(0, "/Applications/Code/Code/py_pnet")
-#import py_pnet
-import py_pnet.py_pnet
+    sniffer = pnet_sniffer.PacketSniffer(interface_name)
 
-print(py_pnet.py_pnet)
+    # Capture 5 packets without any filters
+    print("\nCapturing 5 packets without filters:")
+    packets = sniffer.capture_packets(5)
+    for pkt in packets:
+        print(pkt)
 
-def packet_callback(packet_info):
-    src_mac = packet_info['src_mac']
-    dst_mac = packet_info['dst_mac']
-    ethertype = packet_info['ethertype']
-    payload = packet_info['payload']  # This is bytes
-
-    #print("Packet Received")
-    #print(f"Time: Packet: {src_mac} -> {dst_mac}, Type: {ethertype}, Payload Length: {len(payload)}")
-    print(f"Time: {time.perf_counter()} Packet:  -> {dst_mac}, Type: {ethertype}, Payload Length: {len(payload)}, Data: {payload[:8]}")
-
-# Initialize the sniffer
-sniffer = py_pnet.py_pnet.PacketSniffer("en0")  # Replace "eth0" with your interface
-
-# Start sniffing
-sniffer.sniff(packet_callback)
-
-# Let it run for 10 seconds
-time.sleep(10)
-
-# Stop sniffing
-sniffer.stop()
-
-#py_pnet.py_pnet
+if __name__ == "__main__":
+    main()
